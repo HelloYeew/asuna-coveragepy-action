43b91d5b1d004eba70cfe1ff0d6a73a85a916e3b
Phawit Pornwattanakul <me@helloyeew.dev>
2023-03-08 09:53:24 +0700

    Replace new line with \s

--- asuna.py
+++ asuna.py
@@ -62,4 +62,4 @@ else:
 
     # Write markdown file
     with open('asuna.md', 'w') as f:
-        f.writelines(f'[**📝Project URL**]({output["project_url"]}) | [**🔗Full report URL**]({output["url"]})')
+        f.writelines(f'[**📝Project**]({output["project_url"]}) | [**🔗Full report**]({output["url"]})')
